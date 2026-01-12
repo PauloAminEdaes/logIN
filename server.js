77cf943ccf8ce3689be104c7fd43b7e0dcc7a390
uBuzao <aminedaes@gmail.com>
2026-01-12 20:08:27 -0300

    Iniciando

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Olá do Node.js!");
+  res.end("Olá do Projetinho!");
 });
 
 server.listen(3000, () => {
